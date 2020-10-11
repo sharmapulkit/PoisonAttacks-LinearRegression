b6136d2eb9f871a7e78d923093cb8f7941f49356
sharmapulkit <pulkitsharma285@gmail.com>
2020-10-11 02:06:17 -0400

    ols training fixed

--- models.py
+++ models.py
@@ -7,6 +7,22 @@ class Model():
         self._w = np.ones(d)
         self._b = 1
 
+    @property
+    def w(self):
+        self._w
+
+    @w.setter
+    def w(self, value):
+        self._w = value
+
+    @property
+    def b(self):
+        return self._b
+
+    @b.setter
+    def b(self, value):
+        self._b = value
+
     def mse(self, X, Y):
         """
         return MSE for given data points
@@ -33,31 +49,19 @@ class Model():
         """
         wx = X @ wb[:-1]
         wxb = wx + wb[-1]
-        diff = np.linalg.norm(Y - wxb)
-        return diff
+        diff = np.linalg.norm(Y[:,0] - wxb)
+        mse = np.sum(diff)/X.shape[0]
+        # print("MSE:", mse)
+        return mse
 
     def gradMSE_at(self, wb, X, Y):
         wx = X @ wb[:-1]
         wxb = wx + wb[-1]
-        diff = Y - wxb
-        grad = 2*np.sum(diff * X, axis=1)
+        diff = Y[:,0] - wxb
+        Xd = np.concatenate((X, np.ones(X.shape[0])[:, None]), axis=1)
+        grad = -2*np.sum(diff * Xd.T, axis=1)/X.shape[0]
         return grad
 
-    @property
-    def w(self):
-        self._w
-
-    @w.setter
-    def w(self, value):
-        self._w = value
-
-    @property
-    def b(self):
-        return self._b
-
-    @b.setter
-    def b(self, value):
-        self._b = value
 
 class ols(Model):
     """
@@ -83,12 +87,12 @@ class ols(Model):
         return error
 
     def fit(self, data):
-        X = data.X
-        y = data.Y
+        X = data.X.values
+        y = data.Y.values
 
         theta0 = np.append(self._w, self._b)
         opts = {'maxiter':30, 'disp':True}
-        scipy.optimize.minimize(self.objective_at, theta0, args=(X, y), method='L-BFGS-B', options=opts)
+        scipy.optimize.minimize(self.objective_at, theta0, args=(X, y), method='BFGS', options=opts, jac=self.gradientW_at)
 
 
 class ridge(Model):
